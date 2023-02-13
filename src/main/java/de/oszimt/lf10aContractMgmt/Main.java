58aff3778f6beecc4e549d68fdacbeb69d643cdd
ThatCipher <deniz_malik@outlook.de>
2023-02-13 14:46:00 +0100

    Added Backendfiles from School

---
+++ src/main/java/de/oszimt/lf10aContractMgmt/Main.java
@@ -0,0 +1,10 @@
+package de.oszimt.lf10aContractMgmt;
+
+import de.oszimt.lf10aContractMgmt.impl.HaseGmbHManagement;
+
+public class Main {
+
+    public static void main(String[] args) {
+        HaseGmbHClientSimulation simulation = new HaseGmbHClientSimulation();
+    }
+}
