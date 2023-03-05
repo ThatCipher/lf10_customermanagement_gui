e4ce39039820ca82584e9263cc93b8821c4e3367
jonas <tronic2205@gmail.com>
2023-03-05 14:35:14 +0100

    finished employee view with backend (edit and new employee)

--- src/main/java/de/tectronic/lf10_customermanagement_gui/EmployeeNewEntryController.java
+++ src/main/java/de/tectronic/lf10_customermanagement_gui/EmployeeNewEntryController.java
@@ -1,6 +1,10 @@
 package de.tectronic.lf10_customermanagement_gui;
 
+import de.oszimt.lf10aContractMgmt.impl.HaseGmbHManagement;
+import de.oszimt.lf10aContractMgmt.model.Address;
+import de.oszimt.lf10aContractMgmt.model.Employee;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 public class EmployeeNewEntryController {
@@ -13,16 +17,17 @@ public class EmployeeNewEntryController {
     public TextField txt_plz;
     public TextField txt_email;
     public TextField txt_telephoneNumber;
-
+    public Button btn_addEmployee;
+    HaseGmbHManagement haseGmbHManagement = new HaseGmbHManagement();
     @FXML
-    void addNewEmployee() {
-        // CustomerManagementGUI.client.addNewEmployee(txt_firstName.getText() + txt_lastName.getText());
-        System.out.println(txt_streetName.getText());
-        System.out.println(txt_streetNumber.getText());
-        System.out.println(txt_city.getText());
-        System.out.println(txt_country.getText());
-        System.out.println(txt_plz.getText());
-        System.out.println(txt_email.getText());
-        System.out.println(txt_telephoneNumber.getText());
+    void addEmployee() {
+        System.out.println(haseGmbHManagement.getAllEmployees().size());
+
+        Address address = new Address(txt_streetName.getText(), txt_streetNumber.getText(), txt_plz.getText(), txt_city.getText(), txt_country.getText());
+        haseGmbHManagement.addNewEmployee(new Employee(txt_firstName.getText(), txt_lastName.getText(), address, txt_email.getText(), txt_telephoneNumber.getText()));
+
+        System.out.println(haseGmbHManagement.getAllEmployees().size());
+
+        System.out.println(haseGmbHManagement.getAllEmployees());
     }
 }
