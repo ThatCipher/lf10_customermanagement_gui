e9130bbcc02e33736b3d97d9754cc0591da1f8f8
jonas <tronic2205@gmail.com>
2023-03-05 20:46:11 +0100

    connect employee views

--- src/main/java/de/tectronic/lf10_customermanagement_gui/EmployeeMgmtController.java
+++ src/main/java/de/tectronic/lf10_customermanagement_gui/EmployeeMgmtController.java
@@ -4,9 +4,14 @@ import de.oszimt.lf10aContractMgmt.model.Employee;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class EmployeeMgmtController {
@@ -41,4 +46,29 @@ public class EmployeeMgmtController {
         lsv_employeeList.setCellFactory(employeeListView -> new EmployeeListCellController());
     }
 
+    @FXML
+    void openNewEmployeeWindow(){
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("new-employee-view.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("Auftragsverwaltung - Mitarbeiter hinzufügen");
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    @FXML
+    void openEditEmployeeWindow(){
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("edit-employee-view.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("Auftragsverwaltung - Mitarbeiter hinzufügen");
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
 }
