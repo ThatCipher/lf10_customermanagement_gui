c1331dc523d4ad73b385e4d390e8254df09d54e9
ThatCipher <deniz_malik@outlook.de>
2023-05-23 11:00:32 +0200

    added confirmation alerts for delete interaction of lists + fixed missing delete button on customer view + added delete functionality for customer + added confirm alert

--- src/main/java/de/tectronic/lf10_customermanagement_gui/Util.java
+++ src/main/java/de/tectronic/lf10_customermanagement_gui/Util.java
@@ -2,6 +2,8 @@ package de.tectronic.lf10_customermanagement_gui;
 
 import javafx.event.ActionEvent;
 import javafx.scene.Node;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 
 public class Util {
@@ -10,6 +12,21 @@ public class Util {
         final Node source = (Node) event.getSource();
         final Stage stage = (Stage) source.getScene().getWindow();
         stage.close();
+    }
+
+    public static boolean showConfirmationFieldAlert(){
+        Alert confirmationAlert = new Alert(Alert.AlertType.CONFIRMATION);
+        confirmationAlert.setTitle("Löschen?");
+        confirmationAlert.setHeaderText(null);
+        confirmationAlert.setContentText("Sicher, dass sie den Eintrag löschen wollen?");
+        confirmationAlert.getButtonTypes().clear();
+        confirmationAlert.getButtonTypes().addAll(ButtonType.YES, ButtonType.NO);
+        confirmationAlert.showAndWait();
+
+        if (confirmationAlert.getResult() == ButtonType.YES) {
+            return true;
+        }
+        return false;
     }
 
 }
